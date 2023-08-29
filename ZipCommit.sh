af47dc1d658ae54ecad824d8eece33c631ebe90d
abelclaros <abelclaros@gmail.com>
2023-08-29 09:17:00 -0400

    Script to create zip file updated and corrected to create the zip file in the same folder

--- ZipCommit.sh
+++ ZipCommit.sh
@@ -3,7 +3,7 @@
 # Define the paths to the files and the destination zip file
 file1_path="src/test/resources/edu/ncsu/csc326/coffeemaker/CoffeeMaker.feature"
 file2_path="src/test/java/edu/ncsu/csc326/coffeemaker/TestSteps.java"
-desktop_path="$HOME/Desktop/combined_files.zip"
+desktop_path="combined_files.zip"
 
 # Create a zip file containing the selected files
 zip "$desktop_path" "$file1_path" "$file2_path"
